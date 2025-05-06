ff88c5fd4d4257f40727423ab7c37bc21a3deabd
Jao Fortin <joao.silva.gaspar@escola.pr.gov.br>
2025-05-06 16:21:37 -0300

    06/05

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-const botoes = document. querySelectorAll\("botao");
+const botoes = document. querySelectorAll\(".botao");
 console.log (botoes);
 
-for(let i=0; i<botoes.length; ii++{
+for(let i=0; i < botoes.length; ii++) {
+
+    for (let j=0<botoes.lenght, j++){
+        botoes [j].classList.remove ("ativo");
+    }
 
     botoes[i].onclick=function(){
         botoes [i].classList.add ("ativo");
